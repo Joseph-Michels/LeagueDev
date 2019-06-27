266862da5543fd04f1030cc33f08d90db9376fe1
JosephM <bobelement4181@gmail.com>
2019-06-26 18:27:39 -0700

    switched to pyrebase

--- firebasetest.py
+++ firebasetest.py
@@ -1,10 +1,30 @@
-from firebase import firebase
+import pyrebase
 
-firebase = firebase.FirebaseApplication('https://leaguedev-57c5d.firebaseio.com/')
+config = {
+	'apiKey': "AIzaSyBkiDeRm6ZKergpdVH_zSBceTLNsLCrMLQ",
+	'authDomain': "leaguedev-57c5d.firebaseapp.com",
+	'databaseURL': "https://leaguedev-57c5d.firebaseio.com",
+	'projectId': "leaguedev-57c5d",
+	'storageBucket': "leaguedev-57c5d.appspot.com",
+	'messagingSenderId': "44032877576",
+	'appId': "1:44032877576:web:df52a4cd48875ecd"
+}
 
+firebase = pyrebase.initialize_app(config)
 print(firebase)
 
-getres = firebase.get('/users', None)
-print(getres)
-getpos = firebase.post('/users', "taiwanlul")
-print(getpos)
+auth = firebase.auth()
+print(auth)
+
+
+email = input("enter your email: ")
+password = input("enter your password: ")
+user = auth.sign_in_with_email_and_password(email, password)
+
+database = firebase.database()
+
+data = {
+    "name": "Mortimer 'Morty' Smith"
+}
+
+results = database.child("users").push(data, user['idToken'])
