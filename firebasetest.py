0b1e3748b41690797963d775b8f8a8b722c22b9a
JosephM <bobelement4181@gmail.com>
2019-06-26 19:27:42 -0700

    more pyrebase tests; take from api and add user['idToken'] param

--- firebasetest.py
+++ firebasetest.py
@@ -27,4 +27,36 @@ data = {
     "name": "Mortimer 'Morty' Smith"
 }
 
-results = database.child("users").push(data, user['idToken'])
+# To save data with a unique, auto-generated, timestamp-based key, use the push() method.
+push_result = database.child("users").push(data, user['idToken'])
+print(push_result)
+
+# To create your own keys use the set() method. The key in the example below is "Morty".
+# Think it just updates if it already exists
+set_result = database.child("users").child("Morty").set(data, user['idToken'])
+print(set_result)
+
+# To update data for an existing entry use the update() method.
+update_result = database.child("users").child("Morty").update({"name": "Mortiest Morty"}, user['idToken'])
+print(update_result)
+
+# To delete data for an existing entry use the remove() method.
+remove_result = database.child("users").child("Morty").remove(user['idToken'])
+print(remove_result)
+
+data = {
+    "users/Morty/": {
+        "name": "Mortimer 'Morty' Smith"
+    },
+    "users/Rick/": {
+        "name": "Rick Sanchez"
+    }
+}
+
+multi_result = database.update(data, user['idToken'])
+print(multi_result)
+
+
+users = database.child("users").get(user['idToken'])
+print(users.val()) # key
+print(users.key()) # val
