29391ec4102b2d7f8367cffe477145ad0f10a632
Joseph Michels <bobelement4181@gmail.com>
2019-07-17 20:17:48 -0700

    Bugfixes: - added user refreshing after login expiration (untested) - added handling of responses without rate limit headers - added more request types - fixed trace name bug

--- riot_requester.py
+++ riot_requester.py
@@ -63,8 +63,17 @@ class Requester:
 		Creates a Requester object with firebase initialized. Also prompts the user for login to firebase.
 		"""
 		self.firebase = pyrebase.initialize_app(CONFIG)
-		self.user = get_user(self.firebase)
+		self._user_init_time = get_time()
+		self._user = get_user(self.firebase)
 
+	def get_user(self):
+		if get_time() > self._user_init_time + float(self._user['expiresIn']):
+			self._user_init_time = get_time()
+			self._user = self._firebase.auth().refresh(self._user['refreshToken'])
+		return self._user
+
+	def get_user_id_token(self):
+		return self.get_user()['idToken']
 
 	def request(self, req_type:str, **req_params_kargs):# -> requests.Response:
 		"""
@@ -88,68 +97,68 @@ class Requester:
 	def _remove_old_timestamps(self, req_type:str):
 		time = get_time()
 
-		app_data = self.get_timestamps('app').get(self.user['idToken']).val()
+		app_data = self.get_timestamps('app').get(self.get_user_id_token()).val()
 		if type(app_data) is OrderedDict:
 			for duration, limit_timestamp_dict in app_data.items():
 				if 'timestamps' in limit_timestamp_dict:
 					for key, timestamp in limit_timestamp_dict['timestamps'].items():
 						if time > timestamp + int(duration):
-							self.get_timestamps('app').child(f"{duration}/timestamps/{key}").remove(self.user['idToken'])
+							self.get_timestamps('app').child(f"{duration}/timestamps/{key}").remove(self.get_user_id_token())
 
-		method_data = self.get_timestamps('method').child(req_type).get(self.user['idToken']).val()
+		method_data = self.get_timestamps('method').child(req_type).get(self.get_user_id_token()).val()
 		if type(method_data) is OrderedDict:
 			for duration, limit_timestamp_dict in method_data.items():
 				if 'timestamps' in limit_timestamp_dict:
 					for key, timestamp in limit_timestamp_dict['timestamps'].items():
 						if time > timestamp + int(duration):
-							self.get_timestamps('method').child(f"{req_type}/{duration}/timestamps/{key}").remove(self.user['idToken'])
+							self.get_timestamps('method').child(f"{req_type}/{duration}/timestamps/{key}").remove(self.get_user_id_token())
 
 
 	def _can_request(self, req_type:str):# -> bool:
 		'''check rate_limits retry_after timestamps'''
 		time = get_time()
 		
-		app_rate_limit_data = self.get_rate_limits('app').get(self.user['idToken']).val()
+		app_rate_limit_data = self.get_rate_limits('app').get(self.get_user_id_token()).val()
 		if type(app_rate_limit_data) is OrderedDict:
 			for key, retry_after_timestamp in app_rate_limit_data.items():
 				if time > retry_after_timestamp: # passed retry after mark, remove rate_limit
-					self.get_rate_limits('app').child(key).remove(self.user['idToken'])
+					self.get_rate_limits('app').child(key).remove(self.get_user_id_token())
 				else:
 					return False
 
-		method_rate_limit_data = self.get_rate_limits('method').child(req_type).get(self.user['idToken']).val()
+		method_rate_limit_data = self.get_rate_limits('method').child(req_type).get(self.get_user_id_token()).val()
 		if type(method_rate_limit_data) is OrderedDict:
 			for key, retry_after_timestamp in method_rate_limit_data.items():
 				if time > retry_after_timestamp: # passed retry after mark, remove rate_limit
-					self.get_rate_limits('method').child(f"{req_type}/{key}").remove(self.user['idToken'])
+					self.get_rate_limits('method').child(f"{req_type}/{key}").remove(self.get_user_id_token())
 				else:
 					return False
 
-		service_rate_limit_data = self.get_rate_limits('service').get(self.user['idToken']).val()
+		service_rate_limit_data = self.get_rate_limits('service').get(self.get_user_id_token()).val()
 		if type(service_rate_limit_data) is OrderedDict:
 			for key, retry_after_timestamp in service_rate_limit_data.items():
 				if time > retry_after_timestamp: # passed retry after mark, remove rate_limit
-					self.get_rate_limits('service').child(key).remove(self.user['idToken'])
+					self.get_rate_limits('service').child(key).remove(self.get_user_id_token())
 				else:
 					return False
 
 		# for service_expo, dont remove because the retry_after is handled
 		# exponentially and needs to be referred back to
-		service_expo_rate_limit_data = self.get_rate_limits('service_expo').get(self.user['idToken']).val()
+		service_expo_rate_limit_data = self.get_rate_limits('service_expo').get(self.get_user_id_token()).val()
 		if type(service_expo_rate_limit_data) is OrderedDict:
 			for key, retry_timestamp_dict in service_expo_rate_limit_data.items():
 				if time <= retry_timestamp_dict['timestamp'] + retry_timestamp_dict['retry_after']:
 					return False
 					
 		'''check number of previous calls'''
-		app_timestamp_data = self.get_timestamps('app').get(self.user['idToken']).val()
+		app_timestamp_data = self.get_timestamps('app').get(self.get_user_id_token()).val()
 		if type(app_timestamp_data) is OrderedDict:
 			for duration, limit_timestamp_dict in app_timestamp_data.items():
 				if 'timestamps' in limit_timestamp_dict and len(limit_timestamp_dict['timestamps']) >= int(limit_timestamp_dict['limit']):
 					trace(f"App: Called {len(limit_timestamp_dict['timestamps'])}/{limit_timestamp_dict['limit']} in {duration}s")
 					return False
 
-		method_timestamp_data = self.get_timestamps('method').child(req_type).get(self.user['idToken']).val()
+		method_timestamp_data = self.get_timestamps('method').child(req_type).get(self.get_user_id_token()).val()
 		if type(method_timestamp_data) is OrderedDict:
 			for duration, limit_timestamp_dict in method_timestamp_data.items():
 				if 'timestamps' in limit_timestamp_dict and len(limit_timestamp_dict['timestamps']) >= int(limit_timestamp_dict['limit']):
@@ -175,25 +184,25 @@ class Requester:
 				trace(f"EXCEEDED RATE LIMIT: {rate_limit_type}, retry after {retry_after}s")
 				
 				if rate_limit_type == 'application':
-					self.get_rate_limits('app').push(retry_after_timestamp, self.user['idToken'])
+					self.get_rate_limits('app').push(retry_after_timestamp, self.get_user_id_token())
 				elif rate_limit_type == 'method':
-					self.get_rate_limits('method').child(req_type).push(retry_after_timestamp, self.user['idToken'])
+					self.get_rate_limits('method').child(req_type).push(retry_after_timestamp, self.get_user_id_token())
 				elif rate_limit_type == 'service':
-					self.get_rate_limits('service').push(retry_after_timestamp, self.user['idToken'])
+					self.get_rate_limits('service').push(retry_after_timestamp, self.get_user_id_token())
 				else:
 					raise Error(f"Unexpected rate_limit_type {rate_limit_type}")
 			else:
 				def get_retry_after_timestamp(d):
 					return d['timestamp'] + d['retry_after']
 				trace(f"EXCEEDED RATE LIMIT: back off exponentially until we receive a 200 response")
-				prev_rate_limits = self.get_rate_limits('service_expo').get(self.user['idToken']).val()
+				prev_rate_limits = self.get_rate_limits('service_expo').get(self.get_user_id_token()).val()
 				highest_retry_after = 1
 				if type(prev_rate_limits) is OrderedDict:
 					for key, retry_timestamp_dict in prev_rate_limits.items():
 						# if timestamp passed
 						if time > get_retry_after_timestamp(retry_timestamp_dict):
 							# remove all passed timestamps
-							self.get_rate_limits('service_expo').child(key).remove(self.user['idToken'])
+							self.get_rate_limits('service_expo').child(key).remove(self.get_user_id_token())
 							this_retry_after = retry_timestamp_dict['retry_after']
 							if this_retry_after > highest_retry_after:
 								highest_retry_after = this_retry_after
@@ -203,7 +212,7 @@ class Requester:
 						
 				print(prev_rate_limits)
 				# add timestamp with doubled retry_after
-				self.get_rate_limits('service_expo').push({'timestamp': time, 'retry_after': 2*highest_retry_after}, self.user['idToken'])
+				self.get_rate_limits('service_expo').push({'timestamp': time, 'retry_after': 2*highest_retry_after}, self.get_user_id_token())
 
 			return None
 		else:
@@ -218,35 +227,39 @@ class Requester:
 		trace(f"    time: {time}")
 		trace(f"    headers: {[key for key in resp_headers]}")
 
-		# gets rate limits in array of "max_calls:duration"
-		app_limits_arr = resp_headers['X-App-Rate-Limit'].split(',')
-		app_limits_dict = {}
-		method_limits_arr = resp_headers['X-Method-Rate-Limit'].split(',')
-		method_limits_dict = {}
-
+		# get rate limits in array of "max_calls:duration"
 		# parses this array of strings
-		for ratio_str in app_limits_arr:
-			colon_idx = ratio_str.index(':')
+		if 'X-App-Rate-Limit' in resp_headers:
+			app_limits_arr = resp_headers['X-App-Rate-Limit'].split(',')
+			app_limits_dict = {}
 
-			max_calls = int(ratio_str[:colon_idx])
-			duration = int(ratio_str[colon_idx+1:])
+			for ratio_str in app_limits_arr:
+				colon_idx = ratio_str.index(':')
 
-			d = {"limit": max_calls, "timestamps/"+self.firebase.database().generate_key():time}
-			app_limits_dict[duration] = d
-			result = self.get_timestamps('app').child(duration).update(d, self.user['idToken'])
-		for ratio_str in method_limits_arr:
-			colon_idx = ratio_str.index(':')
+				max_calls = int(ratio_str[:colon_idx])
+				duration = int(ratio_str[colon_idx+1:])
 
-			max_calls = int(ratio_str[:colon_idx])
-			duration = int(ratio_str[colon_idx+1:])
+				d = {"limit": max_calls, "timestamps/"+self.firebase.database().generate_key():time}
+				app_limits_dict[duration] = d
+				result = self.get_timestamps('app').child(duration).update(d, self.get_user_id_token())
 
-			d = {"limit": max_calls, "timestamps/"+self.firebase.database().generate_key():time}
-			method_limits_dict[duration] = d
-			result = self.get_timestamps('method').child(f"{req_type}/{duration}").update(d, self.user['idToken'])
+			trace("    app: (", " | ".join(f"{d['limit']} in {duration}s" for duration, d in app_limits_dict.items()), ")")
 
-		# debug
-		trace("    app: (", " | ".join(f"{d['limit']} in {duration}s" for duration, d in app_limits_dict.items()), ")")
-		trace(f'    method "{req_type}": (', " | ".join(f"{d['limit']} in {duration}s" for duration, d in method_limits_dict.items()), ")")
+		if 'X-Method-Rate-Limit' in resp_headers:
+			method_limits_arr = resp_headers['X-Method-Rate-Limit'].split(',')
+			method_limits_dict = {}
+
+			for ratio_str in method_limits_arr:
+				colon_idx = ratio_str.index(':')
+
+				max_calls = int(ratio_str[:colon_idx])
+				duration = int(ratio_str[colon_idx+1:])
+
+				d = {"limit": max_calls, "timestamps/"+self.firebase.database().generate_key():time}
+				method_limits_dict[duration] = d
+				result = self.get_timestamps('method').child(f"{req_type}/{duration}").update(d, self.get_user_id_token())
+
+			trace(f'    method "{req_type}": (', " | ".join(f"{d['limit']} in {duration}s" for duration, d in method_limits_dict.items()), ")")
 
 
 	def get_timestamps(self, timestamp_type:str = None):
@@ -281,7 +294,9 @@ class UrlBuilder:
 
 REQ_DICT = {
 	"summoner_info": UrlBuilder("lol/summoner/v4/summoners/by-name/{summoner_name}"),
-	"league_info": UrlBuilder("lol/league/v4/entries/by-summoner/{encrypted_summoner_id}")
+	"league_info": UrlBuilder("lol/league/v4/entries/by-summoner/{encrypted_summoner_id}"),
+	"champion_mastery": UrlBuilder("lol/champion-mastery/v4/champion-masteries/by-summoner/{encrypted_summoner_id}"),
+	"status": UrlBuilder("lol/status/v3/shard-data")
 }
 
 def _get_req_url(req_type:str, **req_params_kargs):
@@ -296,7 +311,7 @@ def _get_req_url(req_type:str, **req_params_kargs):
 
 def get(trace:bool = False):# -> Requester
 	if Requester.instance == None:
-		trace("CREATED NEW SINGLETON REQUESTER INSTANCE\n")
+		print("CREATED NEW SINGLETON REQUESTER INSTANCE\n")
 		Requester.instance = Requester()
 	Requester.instance.update_trace(trace)
 	return Requester.instance
