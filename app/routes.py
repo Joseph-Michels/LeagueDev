0da1421cfe10c544157a79242b4e7490db0418a7
Joseph Michels <bobelement4181@gmail.com>
2019-08-29 13:12:38 -0700

    better routes off /home added favicon

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,44 @@
 import requests
+import os
+from flask import render_template, redirect, url_for, flash, send_from_directory
+
 from app import app
-
 import app.riot_requester_util as req_util
-
-from flask import render_template, redirect, url_for, flash
 
 req_util_dict = {}
 
 @app.route("/")
-def home():
-	return render_template("home.html")
+def root():
+	return redirect(url_for("home"))
 
-@app.route("/<req>", methods=['POST'])
-def loading(req):
-	try:
-		req_util_dict[req] = req_util.get_summoners_rundown('NA', *req.split(','))
-		return redirect(f"/results/{req}")
-	except requests.exceptions.RequestException as e:
-		e_dict = eval(str(e))
-		flash(f'Error {e_dict["status_code"]} requesting "{e_dict["url"]}": "{e_dict["message"]}""')
-		return redirect(url_for('home'))
+@app.route("/home")
+@app.route("/home/<req>", methods=['GET','POST'])
+def home(req=""):
+	print(f'home: "{req}"')
+	if req == "":
+		return render_template("home.html", page_name="Scout Five")
+	else:
+		try:
+			arr = req.split(',')
+			print(arr)
+			req_util_dict[req] = req_util.get_summoners_rundown('NA', *arr)
+			return redirect(f"/results/{req}")
+		except requests.exceptions.RequestException as e:
+			e_dict = eval(str(e))
+			flash(f'Error {e_dict["status_code"]} requesting "{e_dict["url"]}": "{e_dict["message"]}""')
+			return redirect(url_for('home'))
 
 
 @app.route("/results/<req>", methods=['GET', 'POST'])
 def results(req):
 	if req in req_util_dict:
-		return render_template("results.html", result=req_util_dict[req])
+		return render_template("results.html", page_name="Search Results", results=req_util_dict[req])
 	else:
-		flash("dont go directly to /results, doesnt necessarily work yet")
-		return redirect(url_for('home'))
-	
+		return redirect(f"/home/{req}")
+
+@app.route("/favicon.ico")
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 
