938c99cb57974a10b7a3caacf738cea548df8f18
Joseph Michels <bobelement4181@gmail.com>
2020-01-03 22:21:55 -0800

    js test fail http request

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-	try:
-		with open("credentials/form_key.txt", 'r') as f:
-			SECRET_KEY = os.environ.get('SECRET_KEY') or f.readline().rstrip()
-	except FileNotFoundError:
-		raise FileNotFoundError('Missing form key.')
-	
