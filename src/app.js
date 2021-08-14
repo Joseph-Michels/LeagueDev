0a426f1374d2b42f487822312b3a44ec01be8092
Joseph Michels <bobelement4181@gmail.com>
2021-08-14 14:21:51 -0700

    moved images to public folder, added ranked images

--- src/app.js
+++ src/app.js
@@ -7,17 +7,20 @@ const favicon = require('serve-favicon');
 const Database = require('./database/database');
 const MainRouter = require('./routers/mainRouter');
 const GroupRouter = require('./routers/groupRouter');
+// const TestRouter = require('./routers/testRouter');
 
 /***************************************************************************
  | INIT                                                                    |
  ***************************************************************************/
 const app = express();
 app.set('view engine', 'ejs');
-app.use(favicon(__dirname + '/../public/images/favicon.ico'));
+app.use(favicon(__dirname + '/../public/img/favicon.ico'));
+app.use('/img', express.static(__dirname + '/../public/img'));
 
 const database = new Database(useDummyResponses=false, verbose=false);
 const mainRouter = new MainRouter(database, verbose=false).router;
 const groupRouter = new GroupRouter(database, verbose=false).router;
+// const testRouter = new TestRouter(database, verbose=false).router;
 
 /***************************************************************************
  | ROUTING                                                                 |
@@ -38,3 +41,4 @@ app.listen(PORT, (err) => {
 
 app.use('/', mainRouter);
 app.use('/group', groupRouter);
+// app.use('/test', testRouter);
